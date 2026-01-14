4b854e49737051141d5ec2c891cbc203c3176e59
Johnse Chance <jchance.is2cool@gmail.com>
2026-01-14 01:33:46 -0500

    first try at part two of day 3

--- src/get_started/advent3.clj
+++ src/get_started/advent3.clj
@@ -26,3 +26,28 @@
  (apply + )
  )
 
+;;;; Part two
+
+
+(defn top-12v [coll] 
+  (loop [digits [(apply max (subvec coll 0 (- (count coll) 12)))]
+         coll (subvec digits (.indexOf coll (peek digits)))]
+    (if (= (count digits) 12)
+      digits
+      (let [next_digit (apply max (subvec coll (inc (.indexOf coll (peek digits)))))
+            coll (subvec digits (.indexOf coll (peek digits)))]
+        (recur (conj digits next_digit) coll))
+      )
+    )
+  )
+
+(defn get-top-twelve-digits-and-make-12d-num [coll]
+  (make-ddnum (top-12v coll)))
+(top-12v [8 1 8 1 8 1 9 1 1 1 1 2 1 1 1])
+(->>
+ (mapv #(str/split % #"") formatted_input)
+ (mapv (fn [x] (mapv #(parse-long %) x)))
+ (mapv #(get-top-twelve-digits-and-make-12d-num %))
+ (apply +))
+
+
