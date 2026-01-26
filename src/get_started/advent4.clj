32930e20892a3c2e8b89ef40f4fda6b084b0b7a6
Johnse Chance <jchance.is2cool@gmail.com>
2026-01-25 21:46:27 -0500

    Solution for part 2 of day 4 completed
    Created method to make char vector out of the previously formatted inputs. Created a new method to get adjacent in these char vectors. Create a method to get a list of positions of removable rolls. Created a method remove rolls that removes all possible rolls. Created method to count removed rolls from a matrix.

--- src/get_started/advent4.clj
+++ src/get_started/advent4.clj
@@ -42,3 +42,64 @@
   )
 
 (count (filter #(= % true) (get-movable-roll-count formatted_input)))
+
+;;; Part 2
+
+(defn make-char-vector [s-matrix]
+  (->>
+   (mapv #(str/split % #"") s-matrix)
+   (mapv (fn [x] (mapv #(.charAt % 0) x)) )
+   ) 
+  )
+
+;;(def cv-test-input (make-char-vector test-input)) 
+(def cv-formatted-input (make-char-vector formatted_input))
+
+(defn get-adjacent-2 [matrix x y]
+  (for [row [(dec y) y (inc y)]
+        col [(dec x) x (inc x)]
+        :let [row-max (count matrix)
+              idx-max (count (matrix 0))]
+        :when (and (< row row-max) (>= row 0) (< col idx-max) (>= col 0) (not (and (= row y) (= col x))))]
+    ;;(list col row)
+    ((matrix row) col)
+    ))
+
+;;(get-adjacent-2 cv-test-input 0 0) 
+
+(defn get-removable-rolls [matrix]
+  (filter #(not (= % nil)) (for [y (range (count matrix))
+                    x (range (count (matrix 0)))
+                    :when (= ((matrix y) x) \@)]
+                (when (> 4 (count (filter #(= % \@) (get-adjacent-2 matrix x y))))
+                  (list x y))
+                ))
+  )
+
+;;(get-removable-rolls cv-test-input)
+
+(defn remove-rolls [matrix]
+  (let [original-m matrix
+        new-m
+        (loop [matrix matrix
+               removable-rolls (get-removable-rolls matrix)]
+          (if (empty? removable-rolls)
+            matrix
+            (let [[rx ry] (first removable-rolls)]
+              (recur (assoc matrix ry (assoc (matrix ry) rx \X)) (rest removable-rolls)))))]
+    (if (= original-m new-m)
+      new-m
+      (remove-rolls new-m) 
+      )
+    ) 
+  )
+
+(defn count-removed-rolls [matrix]
+  (->>
+   (mapv (fn [x] (filter #(= % \X) x)) matrix)
+   (mapv #(count %) ) 
+   (apply + )
+   ) 
+  )
+;;(count-removed-rolls (remove-rolls cv-test-input))
+(count-removed-rolls (remove-rolls cv-formatted-input))
